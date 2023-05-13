7cc330d8698b232442edbf86ac7ff5d33cca540c
David Ormsbee <dave@axim.org>
2023-05-12 20:46:47 -0400

    chore: update dependencies

--- projects/dev.py
+++ projects/dev.py
@@ -48,7 +48,6 @@ INSTALLED_APPS = (
 
 MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
-
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -56,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
     # Admin-specific
     "django.contrib.admindocs.middleware.XViewMiddleware",
 ]
