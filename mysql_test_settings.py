77e432c8840dd558173dae73e27c4a12c153cad4
Irtaza Akram <irtaza.akram@arbisoft.com>
2024-07-29 11:57:37 +0500

    fix: drop support for python 3.8

--- mysql_test_settings.py
+++ mysql_test_settings.py
@@ -4,7 +4,7 @@ the backend. While the openedx-learning apps should run fine using SQLite, they
 also do some MySQL-specific things around charset/collation settings and row
 compression.
 
-The tox targets for py38-django32 and py38-django42 will use this settings file.
+The tox targets for py311-django42 and py312-django42 will use this settings file.
 For the most part, you can use test_settings.py instead (that's the default if
 you just run "pytest" with no arguments).
 
