f969a138ba4edb10cb7f21cf843de1b742ec0c02
Jillian Vogel <jill@opencraft.com>
2024-07-31 04:40:23 +0930

    feat: adds VersioningHelper.last_publish_log property and test

--- tests/openedx_learning/apps/authoring/components/test_models.py
+++ tests/openedx_learning/apps/authoring/components/test_models.py
@@ -3,13 +3,20 @@ Tests related to the Component models
 """
 from datetime import datetime, timezone
 
+from freezegun import freeze_time
+
 from openedx_learning.apps.authoring.components.api import (
     create_component_and_version,
     get_component,
     get_or_create_component_type,
 )
 from openedx_learning.apps.authoring.components.models import ComponentType
-from openedx_learning.apps.authoring.publishing.api import LearningPackage, create_learning_package, publish_all_drafts
+from openedx_learning.apps.authoring.publishing.api import (
+    LearningPackage,
+    create_learning_package,
+    create_publishable_entity_version,
+    publish_all_drafts,
+)
 from openedx_learning.lib.test_utils import TestCase
 
 
@@ -53,3 +60,80 @@ class TestModelVersioningQueries(TestCase):
 
         # Grabbing the list of versions for this component
         assert list(component.versioning.versions) == [component_version]
+
+    def test_last_publish_log(self):
+        """
+        Test last_publish_log versioning property for published Components.
+        """
+        # This Component will get a couple of Published versions
+        component_with_changes, _ = create_component_and_version(
+            self.learning_package.id,
+            component_type=self.problem_type,
+            local_key="with_changes",
+            title="Component with changes v1",
+            created=self.now,
+            created_by=None,
+        )
+
+        # This Component will only be Published once.
+        component_with_no_changes, _ = create_component_and_version(
+            self.learning_package.id,
+            component_type=self.problem_type,
+            local_key="with_no_changes",
+            title="Component with no changes v1",
+            created=self.now,
+            created_by=None,
+        )
+
+        # Publish first time.
+        published_first_time = datetime(2024, 5, 6, 7, 8, 9, tzinfo=timezone.utc)
+        with freeze_time(published_first_time):
+            publish_all_drafts(self.learning_package.id)
+
+        # Refetch the entities to get latest versions
+        component_with_changes = get_component(component_with_changes.pk)
+        component_with_no_changes = get_component(component_with_no_changes.pk)
+
+        # Fetch the most recent PublishLog for these components
+        first_publish_log_for_component_with_changes = component_with_changes.versioning.last_publish_log
+        first_publish_log_for_component_with_no_changes = component_with_no_changes.versioning.last_publish_log
+
+        # PublishLog for library + both entities should match each other
+        assert (
+            published_first_time ==
+            first_publish_log_for_component_with_changes.published_at ==
+            first_publish_log_for_component_with_no_changes.published_at
+        )
+
+        # Modify component_with_changes
+        create_publishable_entity_version(
+            component_with_changes.publishable_entity.id,
+            version_num=2,
+            title="Component with changes v2",
+            created=self.now,
+            created_by=None,
+        )
+
+        # Publish second time
+        published_second_time = datetime(2024, 5, 6, 7, 8, 9, tzinfo=timezone.utc)
+        with freeze_time(published_second_time):
+            publish_all_drafts(self.learning_package.id)
+
+        # Refetch the entities to get latest versions
+        component_with_changes = get_component(component_with_changes.pk)
+        component_with_no_changes = get_component(component_with_no_changes.pk)
+
+        # Re-fetch the most recent PublishLog for these components
+        next_publish_log_for_component_with_changes = component_with_changes.versioning.last_publish_log
+        next_publish_log_for_component_with_no_changes = component_with_no_changes.versioning.last_publish_log
+
+        # PublishLog for component_with_changes should have been updated
+        assert (
+            published_second_time ==
+            next_publish_log_for_component_with_changes.published_at
+        )
+        # But the component_with_no_changes should still be on the original publish log
+        assert (
+            first_publish_log_for_component_with_no_changes ==
+            next_publish_log_for_component_with_no_changes
+        )
