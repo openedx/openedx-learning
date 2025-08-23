4731b8b1bbf5dbb7a73e04799913876051d7eefa
qasimgulzar <qasim.khokhar52@gmail.com>
2025-08-23 17:16:27 +0500

    fix: remove ellipsis from comment

--- openedx_tagging/core/tagging/models/utils.py
+++ openedx_tagging/core/tagging/models/utils.py
@@ -10,7 +10,7 @@ RESERVED_TAG_CHARS = [
            # e.g. lineage="Earth\tNorth America\tMexico\tMexico City"
     ' > ',  # Used in the search index and Instantsearch frontend to separate tag levels
             # e.g. tags_level3="Earth > North America > Mexico > Mexico City"
-    ';',   # Used in CSV exports to separate multiple tags from the same taxonomy…
+    ';',   # Used in CSV exports to separate multiple tags from the same taxonomy
            # e.g. languages-v1: en;es;fr
 ]
 TAGS_CSV_SEPARATOR = RESERVED_TAG_CHARS[2]
