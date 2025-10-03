426206c447f37338d9c1edfbebe3cc5af1b028e4
Daniel Wong <danieleduardo.wongfa@wgu.edu>
2025-10-03 15:33:23 -0600

    feat: add tmp_delete_learning_package api

--- openedx_learning/apps/authoring/backup_restore/api.py
+++ openedx_learning/apps/authoring/backup_restore/api.py
@@ -4,7 +4,8 @@ Backup Restore API
 import zipfile
 
 from openedx_learning.apps.authoring.backup_restore.zipper import LearningPackageUnzipper, LearningPackageZipper
-from openedx_learning.apps.authoring.publishing.api import get_learning_package_by_key
+from openedx_learning.apps.authoring.publishing import api as publishing_api
+from openedx_learning.apps.authoring.collections import api as collections_api
 
 
 def create_zip_file(lp_key: str, path: str) -> None:
@@ -13,7 +14,7 @@ def create_zip_file(lp_key: str, path: str) -> None:
 
     Can throw a NotFoundError at get_learning_package_by_key
     """
-    learning_package = get_learning_package_by_key(lp_key)
+    learning_package = publishing_api.get_learning_package_by_key(lp_key)
     LearningPackageZipper(learning_package).create_zip(path)
 
 
@@ -23,3 +24,28 @@ def load_dump_zip_file(path: str) -> None:
     """
     with zipfile.ZipFile(path, "r") as zipf:
         LearningPackageUnzipper().load(zipf)
+
+def tmp_delete_learning_package(lp_key: str) -> None:
+    """
+    Deletes a learning package and all its associated data.
+    This is a temporary function to help with testing.
+
+    Can throw a NotFoundError at get_learning_package_by_key
+    """
+    learning_package = publishing_api.get_learning_package_by_key(lp_key)
+    collections = collections_api.get_collections(learning_package.id)
+    for collection in collections:
+        collection.delete()
+    for entity in publishing_api.get_publishable_entities(learning_package):
+        publishing_api.DraftSideEffect.objects.filter(cause__entity=entity).delete()
+        publishing_api.Draft.objects.filter(entity=entity).delete()
+        publishing_api.ContainerVersion.objects.filter(
+            entity_list__entitylistrow__entity=entity,
+        ).delete()
+        publishing_api.EntityList.objects.filter(entitylistrow__entity=entity).delete()
+        publishing_api.EntityListRow.objects.filter(entity=entity).delete()
+        publishing_api.Published.objects.filter(entity=entity).delete()
+        publishing_api.DraftChangeLogRecord.objects.filter(entity=entity).delete()
+        publishing_api.PublishLogRecord.objects.filter(entity=entity).delete()
+        entity.delete()
+    learning_package.delete()
