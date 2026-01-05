76bc3d9e9fc1b4a9e63cac7626a4221670928b18
David Ormsbee <dave@axim.org>
2026-01-04 22:54:39 -0500

    temp: more pylint fixes

--- openedx_learning/apps/authoring/apps.py
+++ openedx_learning/apps/authoring/apps.py
@@ -1,16 +1,29 @@
 """
-Docstring for openedx_learning.apps.authoring.apps
+App Config for our umbrella authoring app.
 """
 from django.apps import AppConfig
 
 
 class AuthoringConfig(AppConfig):
+    """
+    Initialization for all applets must happen in here.
+    """
+
     name = "openedx_learning.apps.authoring"
     verbose_name = "Learning Core > Authoring"
     default_auto_field = "django.db.models.BigAutoField"
     label = "oel_authoring"
 
     def ready(self):
+        """
+        Currently used to register nitialize publishable models.
+
+        May later be used to register signal handlers as well.
+        """
+        # Local imports in AppConfig.ready() are common and expected in Django,
+        # since you can't import models at the top level without an error.
+        #
+        # pylint: disable=import-outside-toplevel
         from .api import register_publishable_models
         from .models import (
             Component, ComponentVersion,
